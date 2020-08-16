7f16e41f8d77770858759736f22d0313b791e0b6
parth <patelparth1973@gmail.com>
2020-08-17 00:18:16 +1000

    tag rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   styles = {
@@ -11,15 +12,25 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={this.getbadgeClass()}>
           {" "}
           {this.ChangeCount()}{" "}
         </span>
         <button className="btn btn-secondary btn-sm">Increament</button>
+        <ul>
+          {" "}
+          {this.state.tags.map((tag) => (
+            <li>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
-
+  getbadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
   ChangeCount() {
     return this.state.count === 0 ? "zero" : this.state.count;
   }
